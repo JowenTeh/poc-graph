d864ad0bff2957985760ddbee559bc3915b04ba1
ryalim <ryalim@deloitte.com>
2023-05-19 15:18:10 +0800

    refactor: code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,40 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <div className="App">
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://reactjs.org" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </div>
-//   )
-// }
-
-// export default App
-
-import Graph from 'graphology';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import NodeInfo from './components/NodeInfo';
 import Board from './components/Board';
 import SearchBar from './components/SearchBar';
-import Suggestion from './components/Suggestion';
-import graphData from './data/graph';
 
 export default function App() {
   const [node, setNode] = useState();
-  const [neighbourNodes, setNeighbourNodes] = useState(new Set());
   const [isDragging, setIsDragging] = useState(false);
-  const [suggestions, setSuggestions] = useState([
-    'Expand 3 levels',
-    'STAYS_AT',
-    'HAS_INFRINGEMENT',
-    'HAS_WORK_PERMIT',
-    'IS_ISSUED_BY',
-    // 'OWNS',
-    // 'LOCATED_AT',
-    // 'EMPLOYS',
-  ]);
-  const [selectedSuggestions, setSelectedSuggestions] = useState([]);
-  const [refGraph] = useState(Graph.from(graphData));
-  const [graph] = useState(new Graph());
-  const [query, setQuery] = useState('Unlicensed manufacture and hawking of curry puffs at Blk 63B, Lengkok Bahru which involves Robiah');
+  const [query, setQuery] = useState('');
   const boardRef = useRef();
 
-  window.graph = graph;
+  const [refGraph, setRefGraph] = useState();
+  const [graph, setGraph] = useState();
 
-  const onSearch = (query) => {
-    setQuery(query);
-
-    // setSuggestions([
-    //   'STAYS_AT',
-    //   'HAS_INFRINGEMENT',
-    // ]);
-  };
+  useEffect(() => {
+    const sampleQuery = 'Unlicensed manufacture and hawking of curry puffs at Blk 63B, Lengkok Bahru which involves Robiah';
+    setQuery(sampleQuery);
+    boardRef.current.changeQuery(sampleQuery);
+  }, []);
 
   return (
     <>
-      {/* <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1> */}
-
       <div className="w-screen h-screen">
         <Board
           className="fixed top-0 left-0"
-          graph={graph}
-          refGraph={refGraph}
           node={node}
           setNode={setNode}
-          neighbourNodes={neighbourNodes}
-          setNeighbourNodes={setNeighbourNodes}
           isDragging={isDragging}
           setIsDragging={setIsDragging}
           query={query}
-          suggestions={suggestions}
-          setSuggestions={setSuggestions}
-          selectedSuggestions={selectedSuggestions}
           ref={boardRef}
+          onRefGraphChange={(g) => setRefGraph(g)}
+          onGraphChange={(g) => setGraph(g)}
         >
         </Board>
-        <SearchBar className="absolute top-6 left-6 w-2/6" query={query} onSearch={onSearch} />
-        {/* {query && graph.order > 1 && <Suggestion className="absolute top-24 left-6 w-2/6" suggestions={suggestions} selectedSuggestions={selectedSuggestions} onSelectedSuggestionsChange={setSelectedSuggestions} />} */}
+        <SearchBar className="absolute top-6 left-6 w-2/6" query={query} setQuery={setQuery} onSearch={(query) => { setQuery(query); boardRef.current.changeQuery(query); }} />
         <div className="absolute top-24 left-6 w-2/6">
-          {/* {node && graph.hasNode(node) && <button className="bg-white rounded-full shadow-md text-xs font-medium px-4 py-1 mr-2 last:mr-0 mb-2 hover:shadow-lg active:bg-blue-700 active:text-white" onClick={() => {boardRef.current.expandByLevel(node, 100)}}>Expand all</button>} */}
           {node && graph.hasNode(node) && <button className="bg-white rounded-full shadow-md text-xs font-medium px-4 py-1 mr-2 last:mr-0 mb-2 hover:shadow-lg active:bg-blue-700 active:text-white" onClick={() => {boardRef.current.expandByLevel(node, 2)}}>Expand 2 levels</button>}
           {!node &&
             <>
@@ -112,10 +46,8 @@ export default function App() {
               <button className="block bg-white rounded-full shadow-md text-xs font-medium px-4 py-1 mr-2 last:mr-0 mb-2 hover:shadow-lg active:bg-blue-700 active:text-white" onClick={() => {boardRef.current.expandByNodes(['p-009', 'p-011', 'l-007'])}}>Step 5 (Derek's relationship)</button>
             </>
           }
-          {/* <button className="bg-white rounded-full shadow-md text-xs font-medium px-4 py-1 mr-2 last:mr-0 mb-2 hover:shadow-lg active:bg-blue-700 active:text-white" onClick={() => {boardRef.current.expandByNodes(['w-001'])}}>Step 5b</button> */}
-          {/* <button className="bg-white rounded-full shadow-md text-xs font-medium px-4 py-1 mr-2 last:mr-0 mb-2 hover:shadow-lg active:bg-blue-700 active:text-white" onClick={() => {boardRef.current.expandByNodes(['b-003', 'w-002', 'w-003', 'p-010'])}}>Extra</button> */}
         </div>
-        {query && node && graph.hasNode(node) && <NodeInfo className={`absolute ${query ? /* 'top-44' */ 'top-36' : 'top-24'} left-6 w-2/6`} node={{degree: graph.degree(node), ...graph.getNodeAttributes(node)}} />}
+        {query && node && graph.hasNode(node) && <NodeInfo className={`absolute ${query ? 'top-36' : 'top-24'} left-6 w-2/6`} node={{degree: graph.degree(node), ...graph.getNodeAttributes(node)}} />}
       </div>
     </>
   )
--- src/components/NodeInfo.jsx
+++ src/components/NodeInfo.jsx
@@ -9,7 +9,7 @@ export default function NodeInfo({ className, node }) {
         <div
           className="bg-white block rounded-md shadow-md">
           <div
-            className="border-b-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
+            className="border-b-2 border-neutral-100 px-6 py-3">
             <Badge text={node.category} color={node.color} />
             <span className='pl-4 font-bold'>
               {node.label}
@@ -19,7 +19,7 @@ export default function NodeInfo({ className, node }) {
           <div className="p-6">
             {
               Object.keys(node)
-                .filter((key) => !['x', 'y', 'size', 'color', 'type', 'image', 'category', 'label'].includes(key))
+                .filter((key) => !['x', 'y', 'size', 'color', 'type', 'image', 'category', 'label', 'renderedAt'].includes(key))
                 .map((key) => (
                   <div key={key} className="flex py-2">
                     <div className="flex-1 font-bold">{key}</div>
